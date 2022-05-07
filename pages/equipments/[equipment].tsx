7a8cb4cf4892c098019b5b2ae81f1e4fa7d2ab97
kobakazu0429 <kobakazu10@gmail.com>
2022-05-08 00:38:25 +0900

    add ui

--- pages/equipments/[equipment].tsx
+++ pages/equipments/[equipment].tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import { fetchEquipments } from "../../requests/fetchEquipments";
 
 import type { ParsedUrlQuery } from "node:querystring";
@@ -12,18 +15,161 @@ interface Props {
 }
 
 const EquipmentPage: NextPage<Props> = (props) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
   return (
-    <div>
-      <ul>
-        {Object.keys(props.equipment).map((e) => (
-          <li key={e}>
-            {/* @ts-ignore */}
-            {e}: {props.equipment[e].toString()}
-          </li>
-        ))}
-      </ul>
-      <Image src={props.image} alt="" width={400} height={400} />
-    </div>
+    <section>
+      <h1 className="sr-only">Checkout</h1>
+
+      <div className="relative mx-auto max-w-screen-2xl">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="py-12 bg-gray-50 md:py-24">
+            <div className="max-w-lg px-4 mx-auto lg:px-8">
+              <Image src={props.image} alt="" width={400} height={400} />
+              <ul>
+                {Object.keys(props.equipment).map((e) => (
+                  <li key={e}>
+                    {/* @ts-ignore */}
+                    {e}: {props.equipment[e].toString()}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div className="py-12 bg-white md:py-24">
+            <div className="max-w-lg px-4 mx-auto lg:px-8">
+              <form className="grid grid-cols-6 gap-4">
+                <div className="col-span-3">
+                  <label
+                    className="block mb-1 text-sm text-gray-600"
+                    htmlFor="start_date"
+                  >
+                    開始時間
+                  </label>
+
+                  <DatePicker
+                    id="start_date"
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date!)}
+                    showTimeSelect
+                    dateFormat="yyyy/MM/dd HH:mm"
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    className="border border-gray-200 rounded-lg w-full text-sm p-2.5"
+                  />
+                </div>
+
+                <div className="col-span-3">
+                  <label
+                    className="block mb-1 text-sm text-gray-600"
+                    htmlFor="end_date"
+                  >
+                    修了時間
+                  </label>
+
+                  <DatePicker
+                    id="end_date"
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date!)}
+                    showTimeSelect
+                    dateFormat="yyyy/MM/dd HH:mm"
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    className="border border-gray-200 rounded-lg w-full text-sm p-2.5"
+                  />
+                </div>
+
+                <div className="col-span-3">
+                  <label
+                    className="block mb-1 text-sm text-gray-600"
+                    htmlFor="last_name"
+                  >
+                    学科
+                  </label>
+
+                  <select
+                    className="border border-gray-200 rounded-lg w-full text-sm p-2.5"
+                    id="country"
+                    name="country"
+                  >
+                    <option>M</option>
+                    <option>E</option>
+                    <option>C</option>
+                    <option>A</option>
+                    <option>教職員</option>
+                  </select>
+                </div>
+
+                <div className="col-span-3">
+                  <label
+                    className="block mb-1 text-sm text-gray-600"
+                    htmlFor="first_name"
+                  >
+                    学年
+                  </label>
+
+                  <select
+                    className="border border-gray-200 rounded-lg w-full text-sm p-2.5"
+                    id="country"
+                    name="country"
+                  >
+                    <option>1年</option>
+                    <option>2年</option>
+                    <option>3年</option>
+                    <option>4年</option>
+                    <option>5年</option>
+                    <option>専攻科1年</option>
+                    <option>専攻科2年</option>
+                    <option>教職員</option>
+                  </select>
+                </div>
+
+                <div className="col-span-6">
+                  <label
+                    className="block mb-1 text-sm text-gray-600"
+                    htmlFor="phone"
+                  >
+                    名前
+                  </label>
+
+                  <input
+                    className="rounded-lg shadow-sm border border-gray-200 w-full text-sm p-2.5"
+                    type="tel"
+                    id="phone"
+                  />
+                </div>
+
+                <div className="col-span-6">
+                  <label
+                    className="block mb-1 text-sm text-gray-600"
+                    htmlFor="email"
+                  >
+                    メールアドレス
+                  </label>
+
+                  <input
+                    className="rounded-lg shadow-sm border border-gray-200 w-full text-sm p-2.5"
+                    type="email"
+                    id="email"
+                  />
+                </div>
+
+                <div className="col-span-6">
+                  <button
+                    className="rounded-lg bg-black text-sm p-2.5 text-white w-full block"
+                    type="submit"
+                  >
+                    予約する
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- pages/equipments/index.tsx
+++ pages/equipments/index.tsx
@@ -1,28 +1,119 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 import { fetchEquipments } from "../../requests/fetchEquipments";
 
+import type { FC } from "react";
 import type { NextPage, GetStaticProps } from "next";
 import type { Equipment } from "../../requests/fetchEquipments";
 
 interface Props {
   equipments: Equipment[];
+  equipmentTypes: Equipment["種別"][];
 }
+
+const Card: FC<{ equipment: Equipment }> = (props) => {
+  return (
+    <Link passHref href={`/equipments/${props.equipment.バーコード}`}>
+      <a className="relative block bg-white">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          loading="lazy"
+          alt="Build Your Own Drone"
+          className="object-contain w-full h-56 lg:h-72"
+          src="/toy-1.jpeg"
+        />
+
+        <div className="p-6">
+          <h5 className="text-lg font-bold">{props.equipment.バーコード}</h5>
+
+          <button
+            name="add"
+            type="button"
+            className="flex items-center justify-center w-full px-8 py-4 mt-4 bg-yellow-500 rounded-sm"
+          >
+            <span className="text-sm font-medium">予約する</span>
+          </button>
+        </div>
+      </a>
+    </Link>
+  );
+};
 
 const EquipmentsHome: NextPage<Props> = (props) => {
   return (
-    <div>
-      <ul>
-        {props.equipments.map((e) => (
-          <li key={e.バーコード}>
-            <Link passHref href={`/equipments/${e.バーコード}`}>
-              <a className="relative block p-8 border border-gray-100 shadow-md rounded-xl w-80">
-                {e.バーコード}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <section>
+      <div className="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:items-start">
+          <Filter equipmentTypes={props.equipmentTypes} />
+
+          <div className="lg:col-span-3">
+            <div className="grid grid-cols-1 gap-px bg-gray-200 border border-gray-200 sm:grid-cols-2 lg:grid-cols-3">
+              {props.equipments.map((e) => (
+                <Card key={e.バーコード} equipment={e} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const Filter: FC<{ equipmentTypes: string[] }> = (props) => {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+  useEffect(() => {
+    if (window) {
+      setIsFilterOpen(window.innerWidth > 1280);
+    }
+  }, []);
+
+  return (
+    <div className="lg:sticky lg:top-4">
+      <details
+        open={isFilterOpen}
+        className="overflow-hidden border border-gray-200 rounded"
+      >
+        <summary className="flex items-center justify-between px-5 py-3 bg-gray-100 lg:hidden">
+          <span className="text-sm font-medium text-center w-full">
+            絞り込みの表示/非表示
+          </span>
+        </summary>
+
+        <form action="" className="border-t border-gray-200 lg:border-t-0">
+          <fieldset>
+            <legend className="block w-full px-5 py-3 text-xs font-medium bg-gray-50">
+              種別
+            </legend>
+
+            <div className="px-5 py-6 space-y-2">
+              {props.equipmentTypes.map((type) => (
+                <div className="flex items-center" key={type}>
+                  <input
+                    id="toy"
+                    type="checkbox"
+                    name="type[toy]"
+                    className="w-5 h-5 border-gray-300 rounded"
+                  />
+
+                  <label htmlFor="toy" className="ml-3 text-sm font-medium">
+                    {type}
+                  </label>
+                </div>
+              ))}
+
+              <div className="pt-2">
+                <button
+                  type="button"
+                  className="text-xs text-gray-500 underline"
+                >
+                  リセット
+                </button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </details>
     </div>
   );
 };
@@ -31,9 +122,13 @@ export default EquipmentsHome;
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
   const equipments = await fetchEquipments();
+  const equipmentTypes = Array.from(
+    new Set(equipments.map((e) => e.種別)).values()
+  );
 
   const props: Props = {
     equipments: equipments,
+    equipmentTypes,
   };
 
   return { props };
